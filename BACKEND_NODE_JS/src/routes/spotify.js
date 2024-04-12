bf5ae796bf941c6cbdd229e52354a46f272b2702
Ken <imken0@outlook.com>
2024-04-12 16:27:42 -0400

    fixed music route to catch invalid parameters before calling third party api

--- BACKEND_NODE_JS/src/routes/spotify.js
+++ BACKEND_NODE_JS/src/routes/spotify.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const db = require('../db/spotify');
 const { checkAccessToken, getSpotifyData, } = require('../utils/spotify');
 
+// Define valid time range options
+const validTimeRanges = ['short_term', 'medium_term', 'long_term'];
+
 router.get('/getMusicData', (async (req, res) => {
     try {
         const { songsTimeRange, artistTimeRange } = req.query;
+
+        if (songsTimeRange && !validTimeRanges.includes(songsTimeRange) || artistTimeRange && !validTimeRanges.includes(artistTimeRange)) {
+            return res.json(400).json({ error: 'Invalid Time Range query parameter' });
+        }
 
         const getToken = await db.getSpotifyToken(); // Fetch documents using database function
         const checkToken = await checkAccessToken(getToken);
